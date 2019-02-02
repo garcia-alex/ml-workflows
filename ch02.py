bebedc12cc47be873161286870229b58e63f5c9e
Tsvetan Kintisheff <kintisheff@gmail.com>
2019-02-02 23:57:27 +0000

    add SVR to evaluations

--- ch02.py
+++ ch02.py
@@ -2,9 +2,6 @@ import os
 
 import numpy as np
 import pandas as pd
-from sklearn.linear_model import LinearRegression
-from sklearn.tree import DecisionTreeRegressor
-from sklearn.ensemble import RandomForestRegressor
 
 from utilities import Data, URL_ROOT, PATH_DATASETS
 from pipeline import (
