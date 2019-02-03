23228b458f0cb9302e0a7d51080936a544f1624d
Tsvetan Kintisheff <kintisheff@gmail.com>
2019-02-03 19:46:17 +0000

    shuffle mnist traiing set

--- ch03.py
+++ ch03.py
@@ -1,5 +1,7 @@
 import os
 import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
 
 from utilities import Data, PATH_DATASETS
 
@@ -7,6 +9,9 @@ MNIST_PATH = os.path.join(PATH_DATASETS, "mnist")
 MNIST_DATASET = 'mnist_784'
 MNIST_KEY_DATA = 'data'
 MNIST_KEY_TARGET = 'target'
+MNIST_LEN_TRAIN = 60000
+MNIST_LEN_TEST = 10000
+MNIST_LEN = MNIST_LEN_TRAIN + MNIST_LEN_TEST
 
 
 def fetch_mnist():
@@ -18,8 +23,8 @@ def fetch_mnist():
     return mnist
 
 
-def process_mnist(mnist):
-    n = 60000
+def extract_mnist(mnist):
+    n = MNIST_LEN_TRAIN
 
     mnist.target = mnist.target.astype(np.int8)
 
@@ -39,8 +44,31 @@ def process_mnist(mnist):
     mnist.target[n:] = mnist.target[test + n]
 
 
+def shuffle_mnist_train(X, y):
+    index = np.random.permutation(len(X))
+    X_ = X[index]
+    y_ = y[index]
+
+    return X_, y_
+
+
+def visualize_mnist(mnist, i):
+    X = mnist[0]
+
+    digit = X[i]
+    image = digit.reshape(28, 28)
+    plt.imshow(image, cmap=matplotlib.cm.binary, interpolation="nearest")
+    plt.axis("off")
+    plt.show()
+
+
 if __name__ == '__main__':
     mnist = fetch_mnist()
-    process_mnist(mnist)
+    extract_mnist(mnist)
 
-    X, y = mnist[MNIST_KEY_DATA], mnist[MNIST_KEY_TARGET]
+    n = MNIST_LEN_TRAIN
+
+    train = (mnist[MNIST_KEY_DATA][:n], mnist[MNIST_KEY_TARGET][:n])
+    test = (mnist[MNIST_KEY_DATA][n:], mnist[MNIST_KEY_TARGET][n:])
+
+    shuffled = shuffle_mnist_train(*train)
