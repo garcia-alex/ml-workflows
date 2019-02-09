14eab5a408a5757cb29e8e49921ce55cb08a0ccc
Tsvetan Kintisheff <kintisheff@gmail.com>
2019-02-09 19:01:15 +0000

    attempt multilabel classification

--- ch03.py
+++ ch03.py
@@ -59,9 +59,32 @@ def visualize_mnist(mnist, i):
 
     digit = X[i]
     image = digit.reshape(28, 28)
+
+    plt.figure(figsize=(1, 1))
     plt.imshow(image, cmap=matplotlib.cm.binary, interpolation="nearest")
     plt.axis("off")
     plt.show()
+
+def visualize_mnist_multi(instances, images_per_row=10, **options):
+    size = 28
+
+    images_per_row = min(len(instances), images_per_row)
+    images = [instance.reshape(size, size) for instance in instances]
+
+    n_rows = (len(instances) - 1) // images_per_row + 1
+    
+    row_images = []
+    n_empty = n_rows * images_per_row - len(instances)
+    images.append(np.zeros((size, size * n_empty)))
+
+    for row in range(n_rows):
+        rimages = images[row * images_per_row : (row + 1) * images_per_row]
+        row_images.append(np.concatenate(rimages, axis=1))
+    
+    image = np.concatenate(row_images, axis=0)
+    
+    plt.imshow(image, cmap = matplotlib.cm.binary, **options)
+    plt.axis("off")
 
 
 if __name__ == '__main__':
