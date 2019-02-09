86f2e3a6844003139c712e64d114ccd9895d656a
Tsvetan Kintisheff <kintisheff@gmail.com>
2019-02-09 20:49:57 +0000

    split ch03 notebooks by topic

--- ch03.py
+++ ch03.py
@@ -46,7 +46,7 @@ def extract_mnist(mnist):
     mnist.target[n:] = mnist.target[test + n]
 
 
-def shuffle_mnist_train(X, y):
+def shuffle_mnist(X, y):
     index = np.random.permutation(len(X))
     X_ = X[index]
     y_ = y[index]
@@ -54,16 +54,15 @@ def shuffle_mnist_train(X, y):
     return X_, y_
 
 
-def visualize_mnist(mnist, i):
-    X = mnist[0]
-
-    digit = X[i]
+def visualize_mnist(features, i):
+    digit = features[i]
     image = digit.reshape(28, 28)
 
     plt.figure(figsize=(1, 1))
     plt.imshow(image, cmap=matplotlib.cm.binary, interpolation="nearest")
     plt.axis("off")
     plt.show()
+
 
 def visualize_mnist_multi(instances, images_per_row=10, **options):
     size = 28
@@ -87,6 +86,43 @@ def visualize_mnist_multi(instances, images_per_row=10, **options):
     plt.axis("off")
 
 
+def prc_graph(p, r, t, show=True):
+    if show:
+        plt.figure(figsize=(8, 4))
+    plt.plot(t, p, 'b--', label="Precision")
+    plt.plot(t, r, 'g-', label="Recall")
+    plt.xlabel("Threshold")
+    plt.legend(loc="center left")
+    plt.ylim([0, 1])
+    plt.xlim([-700000, 700000])
+    if show:
+        plt.show()
+
+
+def pr_graph(p, r, show=True):
+    if show:
+        plt.figure(figsize=(8, 4))
+    plt.plot(r, p, 'b-', label="Precision")
+    plt.xlabel("Recall")
+    plt.legend(loc="center left")
+    plt.ylim([0, 1])
+    plt.xlim([0, 1])
+    if show:
+        plt.show()
+
+
+def roc_graph(fpr, tpr, label=None, show=True):
+    if show:
+        plt.figure(figsize=(8, 4))
+    plt.plot(fpr, tpr, linewidth=2, label=label)
+    plt.plot([0, 1], [0, 1], 'k--')
+    plt.axis([0, 1, 0, 1])
+    plt.xlabel("False Positive Rate (1 - Specificity)")
+    plt.ylabel("True Positive Rate (Sensitivity)")
+    if show:
+        plt.show()
+
+
 if __name__ == '__main__':
     mnist = fetch_mnist()
     extract_mnist(mnist)
@@ -96,11 +132,6 @@ if __name__ == '__main__':
     train = (mnist[MNIST_KEY_DATA][:n], mnist[MNIST_KEY_TARGET][:n])
     test = (mnist[MNIST_KEY_DATA][n:], mnist[MNIST_KEY_TARGET][n:])
 
-    shuffled = shuffle_mnist_train(*train)
-    X_, y_ = shuffled
-
-    y_train = (y_ == 5)
-    y_test = (test[1] == 5)
-
-    sgd_clf = SGDClassifier(max_iter=5, tol=-np.infty, random_state=42)
-    sgd_clf.fit(X_, y_train)
+    shuffled = shuffle_mnist(*train)
+    features, labels = shuffled
+    labels_binary = (labels == 5)
